dc9b3e4cde09891f86e1e611c630806f92f4d947
Nicole Sattler <nicole.sattler@live.com>
2019-09-18 11:30:21 -0500

    Add http package and fetch time data on loading screen

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,16 +7,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  void get_data() {
-    
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    print('init state func ran');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,29 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    String url = 'http://worldtimeapi.org/api/timezone/America/Mexico_City';
+
+    Response response =  await get(url);
+    Map jsonData = jsonDecode(response.body);
+
+    String timezone = jsonData['timezone'];
+    String datetime = jsonData['datetime'];
+    int offset = int.parse(jsonData['utc_offset'].substring(0, 3));
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: offset));
+
+    print(timezone);
+    print(now);
+  }
+
+  @override void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
