b15ff81da93bb7c785289ff1ef1498ff00c93f22
Nicole Sattler <nicole.sattler@live.com>
2019-09-26 10:27:05 -0500

    Update time data when user chooses location

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,14 +10,16 @@ class _HomeState extends State<Home> {
   String backgroundImage;
   Color backgroundColor;
 
-  TextStyle setTextStyle(double fontSize, bool isDaytime) {
+  TextStyle setTextStyle(
+      double fontSize, FontWeight fontWeight, bool isDaytime) {
     return TextStyle(
       color: isDaytime ? Colors.grey[800] : Colors.white,
       fontSize: fontSize,
+      fontWeight: fontWeight,
       shadows: [
         Shadow(
           blurRadius: 8.0,
-          color: isDaytime ? Colors.grey[600] : Colors.grey[800],
+          color: isDaytime ? Colors.grey[500] : Colors.grey[800],
           offset: Offset(0.0, 2.0),
         ),
       ],
@@ -26,7 +28,10 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    timeData = ModalRoute.of(context).settings.arguments;
+    timeData = timeData.isNotEmpty
+        ? timeData
+        : ModalRoute.of(context).settings.arguments;
+
     backgroundImage = timeData['isDaytime'] ? 'day.jpg' : 'night.jpg';
     backgroundColor =
         timeData['isDaytime'] ? Colors.purpleAccent : Colors.blue[800];
@@ -42,8 +47,13 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             FlatButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
+                onPressed: () async {
+                  dynamic newTimeData =
+                      await Navigator.pushNamed(context, '/location');
+
+                  setState(() {
+                    timeData = newTimeData;
+                  });
                 },
                 icon: Icon(Icons.edit_location),
                 label: Text('Edit Location')),
@@ -55,14 +65,15 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Text(
                   timeData['location'],
-                  style: setTextStyle(22.0, timeData['isDaytime']),
+                  style: setTextStyle(
+                      20.0, FontWeight.normal, timeData['isDaytime']),
                 ),
               ],
             ),
             SizedBox(height: 20),
             Text(
               timeData['time'],
-              style: setTextStyle(56.0, timeData['isDaytime']),
+              style: setTextStyle(56.0, FontWeight.bold, timeData['isDaytime']),
             ),
           ],
         ),
